0f726dc24380b190ed5da1e35f59d77df18215fe
Danielle Dempsey <ddempsey@perennia.ca>
2021-11-29 11:44:24 -0400

    feat: add option to trim rows with NA for SPEED or DIRECTION

--- R/adcp_format_opendata.R
+++ R/adcp_format_opendata.R
@@ -2,16 +2,19 @@
 #'
 #'
 #' @param dat Output from  \code{adcp_read_txt()} or \code{adcp_assign_alt()}.
+
+#' @param rm_NA Logical argument. If \code{rm_NA = TRUE}, rows where \code{SPEED}
+#'   is \code{NA} OR \code{DIRECTION} is \code{NA} will be removed.
 #'
 #' @return Fill this in
 #'
-#' @importFrom dplyr all_of filter left_join rename select
+#' @importFrom dplyr all_of filter left_join mutate rename select
 #' @importFrom tidyr pivot_longer pivot_wider
 #'
 #' @export
 
 
-adcp_format_opendata <- function(dat){
+adcp_format_opendata <- function(dat, rm_NA = TRUE){
 
   index <- find_index(dat)
 
@@ -19,13 +22,18 @@ adcp_format_opendata <- function(dat){
     filter(VARIABLE == "SensorDepth") %>%
     select(TIMESTAMP, SENSOR_DEPTH = all_of(index))
 
-  dat %>%
+  dat <- dat %>%
     filter(VARIABLE != "SensorDepth") %>%
     select(-Num) %>%
     pivot_longer(cols = (all_of(index)-1):last_col(),
                  names_to = "BIN_ALTITUDE", values_to = "VALUE") %>%
     pivot_wider(names_from = "VARIABLE", values_from = VALUE) %>%
     left_join(sensor_depth, by = "TIMESTAMP") %>%
-    rename(SPEED = WaterSpeed, DIRECTION = WaterDirection)
+    rename(SPEED = WaterSpeed, DIRECTION = WaterDirection) #%>%
+   # mutate(TIMESTAMP = as.character(TIMESTAMP))
+
+  if(rm_NA) dat <- filter(dat, !is.na(SPEED) & !is.na(DIRECTION))
+
+  dat
 
 }
