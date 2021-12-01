d0deeef36de315a7ce8d33c1c97b68d4c46f1865
Danielle Dempsey <ddempsey@perennia.ca>
2021-12-01 12:04:14 -0400

    docs: update documentation.
    use dat_wide instead of dat to clarify the required format of the input
    
    remove rows that have NA for SPEED *OR* DIRECTION

--- R/adcp_format_opendata.R
+++ R/adcp_format_opendata.R
@@ -1,12 +1,13 @@
 #' Convert ADCP data from raw format to Open Data Portal format
 #'
 #'
-#' @param dat Output from  \code{adcp_read_txt()} or \code{adcp_assign_alt()}.
+#' @param dat_wide Dataframe of ADCP data, as returned from
+#'   \code{adcp_read_txt()} or \code{adcp_assign_alt()}.
 
 #' @param rm_NA Logical argument. If \code{rm_NA = TRUE}, rows where \code{SPEED}
 #'   is \code{NA} OR \code{DIRECTION} is \code{NA} will be removed.
 #'
-#' @return Fill this in
+#' @return Returns data in a long format, with columns:
 #'
 #' @importFrom dplyr all_of filter left_join mutate rename select
 #' @importFrom tidyr pivot_longer pivot_wider
@@ -14,23 +15,23 @@
 #' @export
 
 
-adcp_format_opendata <- function(dat, rm_NA = TRUE){
+adcp_format_opendata <- function(dat_wide, rm_NA = TRUE){
 
-  index <- find_index(dat)
+  index <- find_index(dat_wide)
 
-  sensor_depth <- dat %>%
+  sensor_depth <- dat_wide %>%
     filter(VARIABLE == "SensorDepth") %>%
     select(TIMESTAMP, SENSOR_DEPTH = all_of(index))
 
-  dat <- dat %>%
+  dat <- dat_wide %>%
     filter(VARIABLE != "SensorDepth") %>%
     select(-Num) %>%
+    # use index - 1 because dropped the Num column
     pivot_longer(cols = (all_of(index)-1):last_col(),
                  names_to = "BIN_ALTITUDE", values_to = "VALUE") %>%
     pivot_wider(names_from = "VARIABLE", values_from = VALUE) %>%
     left_join(sensor_depth, by = "TIMESTAMP") %>%
     rename(SPEED = WaterSpeed, DIRECTION = WaterDirection)
-   # mutate(TIMESTAMP = as.character(TIMESTAMP))
 
   if(suppressWarnings(!is.na(as.numeric(dat$BIN_ALTITUDE[1])))) {
 
@@ -39,7 +40,7 @@ adcp_format_opendata <- function(dat, rm_NA = TRUE){
 
   }
 
-  if(rm_NA) dat <- filter(dat, !is.na(SPEED) & !is.na(DIRECTION))
+  if(rm_NA) dat <- filter(dat, !is.na(SPEED) | !is.na(DIRECTION))
 
   dat
 
