3c9ed1387dcff75d077b3fcc3c11bcdbb6b4e48d
Danielle Dempsey <ddempsey@perennia.ca>
2022-05-18 14:24:50 -0300

    feat: made more flexible so can apply to the document history table

--- R/adcp_format_report_table.R
+++ R/adcp_format_report_table.R
@@ -3,6 +3,11 @@
 #' @param report_table Deployment metadata from the NSDFA tracking sheet, as
 #'   returned from \code{adcp_write_report_table()}.
 #'
+#' @param transpose Logical argument indicating whether to transpose
+#'   \code{report_table} before applying the format. Use the default \code{TRUE}
+#'   for the deployment table. Set to \code{FALSE} for the document history
+#'   table.
+#'
 #' @return Returns a flextable object that will render nicely in the Word
 #'   report. A table with two columns: the first column (bold) is the column
 #'   names of \code{report_table}; the second column is the corresponding
@@ -16,30 +21,40 @@
 #' @export
 #'
 
-adcp_format_report_table <- function(report_table){
+adcp_format_report_table <- function(report_table, transpose = TRUE){
 
-  # transpose table and make the column names the first column
-  t_table <- report_table %>%
-    t() %>%
-    data.frame() %>%
-    mutate(Record = colnames(report_table)) %>%
-    select(Record, 'Deployment Info' = 1)
-  rownames(t_table) <- NULL
+  if(isTRUE(transpose)){
+    # transpose table and make the column names the first column
+    report_table <- report_table %>%
+      t() %>%
+      data.frame() %>%
+      mutate(Record = colnames(report_table)) %>%
+      select(Record, 'Deployment Info' = 1)
+    rownames(report_table) <- NULL
+
+    report_table <- report_table %>%
+      flextable::flextable() %>%
+      flextable::delete_part(part = "header") %>%
+      flextable::bold(i = NULL, j = 1, bold = TRUE, part = "body")
+
+  } else {
+
+    report_table <- report_table %>%
+      flextable::flextable() %>%
+      flextable::bold(bold = TRUE, part = "header")
+  }
 
   # set border style
   small_border <- officer::fp_border(color = "gray", width = 1)
 
   # format table
-  t_table %>%
-    flextable::flextable() %>%
-    flextable::delete_part(part = "header") %>%
+  report_table %>%
     # borders
     flextable::vline(border = small_border, part = "all" )%>%
     flextable::border_outer(part = "all", border = small_border )%>%
     flextable::border_inner_h(part = "all", border = small_border )%>%
     flextable::border_inner_v(part = "all", border = small_border )%>%
     # font
-    flextable::bold(i = NULL, j = 1, bold = TRUE, part = "body") %>%
     flextable::font(part = "all", fontname = "ebrima") %>%
     flextable::fontsize(size = 10, part = "all") %>%
     # fit
