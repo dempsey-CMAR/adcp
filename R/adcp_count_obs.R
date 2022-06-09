7bef92e8582f62e27b72e0d08d9bdbd151e9a16f
Danielle Dempsey <ddempsey@perennia.ca>
2022-06-09 08:12:08 -0300

    feat: add column argument to make function more flexible
    fix: streamline str_remove

--- R/adcp_count_obs.R
+++ R/adcp_count_obs.R
@@ -1,58 +1,82 @@
 #' Count number and proportion of observations in equal intervals
 #'
-#' @details Uses the \code{cut()} function to assign intervals to
-#'   \code{dat_speed}. Intervals are right-inclusive, such that a value of 16.8
-#'   is assigned to the interval (11.2, 16.8). Note that the interval break
-#'   points use more decimals than are displayed. For example, the value 39.1
-#'   may be assigned to the interval (39.1, 44.6), which appears counter to the
-#'   previous example. However, increasing \code{n_digits} to 4, the interval is
-#'   displayed as (39.07, 44.63).
+#' @details Assigns intervals to \code{column} using the \code{cut()} function.
+#'   Argument \code{right = TRUE} to match the intervals assigned in the
+#'   \code{openair::windRose()} function (which is called in
+#'   \code{adcp_plot_current_rose()}). This means intervals are right-inclusive,
+#'   i.e., a value of 4 is assigned to the interval (1,4].
 #'
-#' @param dat_speed Data observations to assign intervals. The interval size is
-#'   determined by the range of these observations and \code{n_ints}.
+#'   NOTE: The \code{openair::windRose()} function  has the \code{cut()}
+#'   argument \code{include.lower} hard-coded to \code{FALSE}. This causes an
+#'   issue with the intervals extracted from \code{adcp_count_obs()} being
+#'   passed to \code{adcp_plot_current_rose()} (which passes all arguments to
+#'   \code{openair::windRose()}).
+#'
+#'   Assume the first interval is in the form (x1, x2], and the minimum value of
+#'   the observations is xmin.
+#'
+#'   This means that if the round(x1, digits = n_digits) equals xmin, values of
+#'   xmin will NOT be assigned to an interval. In this case, xmin falls outside
+#'   of the first interval (because it is left exclusive). To avoid this,
+#'   n_digits should be large enough to ensure round(x1, digits = n_digits) <
+#'   xmin. This could also be solved if \code{include.lower} could be set to
+#'   \code{TRUE} in \code{openair::windRose()}.
+#'
+#' @param dat Dataframe including the column with observations that will be
+#'   assigned to intervals. The interval size is determined by the range of
+#'   these observations and \code{n_ints}.
+#'
+#' @param column Column in \code{dat} that will be assigned to intervals for
+#'   frequency table (NOT QUOTED).
 #'
 #' @param n_ints Number of intervals to divide observations into. The interval
 #'   size is determined by the range of these observations and \code{n_ints}.
+#'   Alternatively, a vector of two or more unique break points. Passed to the
+#'   \code{breaks} argument of \code{cut()}.
 #'
-#' @param n_digits Number of digits to show in interval limits.
+#' @param n_digits Number of digits to show in interval labels. Note that
+#'   additional digits may be used in the actual break values. See vignettes for
+#'   more detail. Passed to the \code{dig.lab} argument of \code{cut()}.
+#'
+#' @param lowest Logical. If \code{TRUE}, the first interval will be left *and*
+#'   right inclusive. Passed to the \code{include.lowest} argument of
+#'   \code{cut()}.
 #'
 #' @param label_sep Separator for the interval labels ("lower to upper").
 #'   Default is a new line to save room on plot axis.
 #'
-#' @return Returns a dataframe of upper and lower interval limits (lower
+#' @return Returns a dataframe of lower and upper interval limits (right
 #'   inclusive), and frequency and proportion of observations in each interval.
 #'
-#' @importFrom dplyr arrange mutate select tibble
+#' @importFrom dplyr arrange mutate pull select tibble
 #'
 #' @export
 
 adcp_count_obs <- function(
-  dat_speed,
+  dat,
+  column = SPEED,
   n_ints = 12,
-  n_digits = 3,
-  label_sep = "\n"
+  n_digits =3,
+  label_sep = "\n",
+  lowest = FALSE
 ){
 
-  if(ncol(data.frame(dat_speed)) > 1){
-
-    stop("Multiple columns detected in dat_speed.\nHINT: input only the column to be binned")
-
-  }
-
   # frequency table with n_ints even bins
-  ints <- cut(
-    dat_speed,
-    breaks = n_ints,
-    dig.lab = n_digits,
-    right = FALSE,
-    include.lowest = TRUE
-  ) %>%
+  ints <- dat %>%
+    pull({{ column }}) %>%
+    cut(
+      breaks = n_ints,
+      dig.lab = n_digits,
+      right = TRUE,            # for consistency with the openair::windRose
+      include.lowest = lowest
+    ) %>%
     table() %>%
     data.frame() %>%
     # extract lower and upper bin limits
     separate(col = 1, into = c("lower", "upper"), sep = ",") %>%
     mutate(
-      lower = as.numeric(str_remove(lower, pattern = "\\["))
+      lower = as.numeric(str_remove(lower, pattern = "\\(|\\[|\\)|\\]")),
+      upper = as.numeric(str_remove(upper, pattern = "\\(|\\[|\\)|\\]"))
     )
 
   # to avoid negative intervals and very small start intervals
@@ -67,8 +91,7 @@ adcp_count_obs <- function(
   # create bin label and calculate proportion
   ints <- ints %>%
     mutate(
-      upper = str_remove(upper, pattern = "\\)"),
-      upper = as.numeric(str_remove(upper, pattern = "\\]")),
+      #ints_label = glue("{round(lower, digits = 1)}{label_sep}to{round(upper, digits = 1)}"),
       ints_label = paste(lower, "to", upper, sep = label_sep),
       prop = Freq / sum(Freq) * 100
     ) %>%
