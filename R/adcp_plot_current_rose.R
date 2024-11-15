c72f03f2bbbae00a178acc2541284516f5d2c32d
Danielle Dempsey <ddempsey@perennia.ca>
2024-11-15 16:02:34 -0400

    hard code location of direction labels

--- R/adcp_plot_current_rose.R
+++ R/adcp_plot_current_rose.R
@@ -24,6 +24,8 @@
 #' @param speed_label Title of the current speed legend. Default is "Current
 #'   Speed (cm/s)".
 #'
+#' @param add_dir_labs placeholder
+#'
 #' @return Returns an "openair" object, a rose plot of current speed and
 #'   direction.
 #'
@@ -40,8 +42,8 @@ adcp_plot_current_rose <- function(
     breaks,
     speed_column, direction_column,
     speed_colors = NULL,
-    speed_label = "Current Speed (cm/s)"
-  #  add_direction_labs = TRUE,
+    speed_label = "Current Speed (cm/s)",
+    add_dir_labs = TRUE
 ) {
   if (is.null(speed_colors)) {
     speed_colors <- viridis(breaks, option = "F", direction = -1)
@@ -50,7 +52,7 @@ adcp_plot_current_rose <- function(
   dat <- dat %>%
     select(SPEED = {{ speed_column }}, DIRECTION = {{ direction_column }})
 
- p <- openair::windRose(
+  p <- openair::windRose(
     dat,
     ws = "SPEED", wd = "DIRECTION",
     breaks = breaks,
@@ -64,29 +66,22 @@ adcp_plot_current_rose <- function(
     plot = FALSE
   )
 
-#   if(isTRUE(add_direction_labs)) {
-#
-# #    browser()
-#
-#     direction_label_placement <- direction_label_placement
-#
-#     #ints <- adcp::adcp_count_obs(dat, SPEED, n_ints = length(breaks))
-#     #dir_cor <- round(max(ints$prop)) # non-standard evaluation so this doesn't work
-#
-#     p <- p$plot +
-#       latticeExtra::layer(
-#         lattice::ltext(0, direction_label_placement, "N", cex = 0.75, col = "darkgrey")
-#       ) +
-#     latticeExtra::layer(
-#       lattice::ltext(round(max(ints$prop)), 0, "E", cex = 0.75, col = "darkgrey")
-#     ) +
-#     latticeExtra::layer(
-#       lattice::ltext(-round(max(ints$prop)), 0, "W", cex = 0.75, col = "darkgrey")
-#     ) +
-#     latticeExtra::layer(
-#       lattice::ltext(0, -round(max(ints$prop)), "S", cex = 0.75, col = "darkgrey")
-#     )
-#   }
+  if(isTRUE(add_dir_labs)) {
+
+    p <- p$plot +
+      latticeExtra::layer(
+        lattice::ltext(0, 5, "N", cex = 0.75, col = "darkgrey")
+      ) +
+      latticeExtra::layer(
+        lattice::ltext(5, 0, "E", cex = 0.75, col = "darkgrey")
+      ) +
+      latticeExtra::layer(
+        lattice::ltext(-5, 0, "W", cex = 0.75, col = "darkgrey")
+      ) +
+      latticeExtra::layer(
+        lattice::ltext(0, -5, "S", cex = 0.75, col = "darkgrey")
+      )
+  }
 
   p
 
