1a52e1c59cf7f36e476a9a9eb550da2e35afd50f
Danielle Dempsey <ddempsey@perennia.ca>
2025-03-26 13:29:06 -0300

    remove option to read in other sheets because columns are specified for Deployment Information.
    Could add back in with character vector of columns for each sheet

--- R/adcp_read_tracking.R
+++ R/adcp_read_tracking.R
@@ -1,41 +1,33 @@
 #' Read deployment details from CMAR tracking sheet
 #'
-#' @param link Link to the adcp tracking sheet on Google Drive.
+#' @param link Link to the Current & Wave Tracking Deployment Details sheet on
+#'   Google Drive.
 #'
-#' @param sheet Character string with the name of the sheet to read in or an
-#'   abbreviation of the sheet name. \code{sheet = "details"} will read in the
-#'   sheet "Deployment Details", \code{sheet = "current"} will read in sheet
-#'   "Current Tracking", and \code{sheet = "wave"} will read in sheet
-#'   "Wave Tracking", .
-#'
-#' @importFrom dplyr select
 #' @importFrom googlesheets4 gs4_deauth read_sheet
-#' @importFrom lubridate with_tz
-#' @importFrom stringr str_detect
 #'
-#' @return Returns a data frame of the calval tracking sheet. Deployment and
-#'   retrieval datetimes are appended in "Canada/Atlantic" and "UTC" timezones.
+#' @return Returns a data frame of the Deployment Details sheet.
+#'
 #' @export
 
 
-adcp_read_tracking <- function(link = NULL, sheet = "details") {
+adcp_read_tracking <- function(link = NULL) {
 
   if(is.null(link)) {
     link <- "https://docs.google.com/spreadsheets/d/1DVfJbraoWL-BW8-Aiypz8GZh1sDS6-HtYCSrnOSW07U/edit?gid=496612477#gid=496612477"
   }
 
-  sheet <- tolower(sheet)
+  #sheet <- tolower(sheet)
 
-  if(str_detect(sheet, "details")) sheet <- "Deployment Details"
-  if(str_detect(sheet, "current")) sheet <- "Current Tracking"
-  if(str_detect(sheet, "wave")) sheet <- "Wave Tracking"
+  # if(str_detect(sheet, "details")) sheet <- "Deployment Details"
+  # if(str_detect(sheet, "current")) sheet <- "Current Tracking"
+  # if(str_detect(sheet, "wave")) sheet <- "Wave Tracking"
 
   googlesheets4::gs4_deauth()
 
   googlesheets4::read_sheet(
     link,
-    sheet = sheet,
-    col_types = "cccccTTnnncnnnnnnnnnnn",
+    sheet = "Deployment Details",
+    col_types = "cccccTTnnncnnnnnnnnnnnc",
     na = c("", "NA")
   )
 }
