e16f8a3d96e87187c8409c09c843fcd26e40600f
Danielle Dempsey <ddempsey@perennia.ca>
2025-11-28 15:50:11 -0400

    keep as a reminder to update with new google sheet

--- R/adcp_compile_deployment_info.R
+++ R/adcp_compile_deployment_info.R
@@ -1,5 +1,7 @@
 #' Export ADCP Data Deployment Information Dataset
 #'
+#' TO DO: re-write this with new tracking sheet
+#'
 #' @description Imports NSDFA Tracking sheet and ADCP TRACKING and exports the
 #'   metadata for the Nova Scotia Open Data Portal.
 #'
@@ -20,36 +22,36 @@
 
 adcp_compile_deployment_info <- function(path_nsdfa = NULL, sheet = NULL) {
 
-  # import files ------------------------------------------------------------
-  if(is.null(path_nsdfa)) {
-    path_nsdfa <- "R:/tracking_sheets/2023-11-27 - NSDFA Tracking Sheet.xlsx"
-  }
-
-  if(is.null(sheet)) sheet <- "CurrMetaData"
-
-  adcp_read_nsdfa_metadata(path_nsdfa, sheet = sheet) %>%
-    arrange(County, Waterbody, Depl_Date) %>%
-    transmute(
-      county = County,
-      waterbody = Waterbody,
-      station = Station_Name,
-      deployment_date = as_date(Depl_Date),
-      recovery_date = as_date(Recv_Date),
-      deployment_duration_days = Depl_Duration,
-      lease = `Lease#`,
-      latitude = Depl_Lat,
-      longitude = Depl_Lon,
-      sensor_model = Inst_Model,
-      sensor_serial_number = Inst_Serial,
-      deployment_sounding_m = Depl_Sounding,
-      sensor_height_above_sea_floor_m = Inst_Altitude,
-      first_bin_range_m = First_Bin_Range,
-      bin_size_m = Bin_Size,
-      current_ensemble_interval_s = Current_Ensemble_Interval_s,
-      current_averaging_interval_s = Current_Averaging_Interval_s,
-      current_pings_per_ensemble = Current_PingsPerEnsemble,
-      wave_ensemble_interval_s = Waves_Ensemble_Interval_s,
-      wave_averaging_interval_s = Waves_Averaging_Interval_s,
-      wave_pings_per_ensemble = Waves_PingsPerEnsemble
-    )
+  # # import files ------------------------------------------------------------
+  # if(is.null(path_nsdfa)) {
+  #   path_nsdfa <- "R:/tracking_sheets/2023-11-27 - NSDFA Tracking Sheet.xlsx"
+  # }
+  #
+  # if(is.null(sheet)) sheet <- "CurrMetaData"
+  #
+  # adcp_read_nsdfa_metadata(path_nsdfa, sheet = sheet) %>%
+  #   arrange(County, Waterbody, Depl_Date) %>%
+  #   transmute(
+  #     county = County,
+  #     waterbody = Waterbody,
+  #     station = Station_Name,
+  #     deployment_date = as_date(Depl_Date),
+  #     recovery_date = as_date(Recv_Date),
+  #     deployment_duration_days = Depl_Duration,
+  #     lease = `Lease#`,
+  #     latitude = Depl_Lat,
+  #     longitude = Depl_Lon,
+  #     sensor_model = Inst_Model,
+  #     sensor_serial_number = Inst_Serial,
+  #     deployment_sounding_m = Depl_Sounding,
+  #     sensor_height_above_sea_floor_m = Inst_Altitude,
+  #     first_bin_range_m = First_Bin_Range,
+  #     bin_size_m = Bin_Size,
+  #     current_ensemble_interval_s = Current_Ensemble_Interval_s,
+  #     current_averaging_interval_s = Current_Averaging_Interval_s,
+  #     current_pings_per_ensemble = Current_PingsPerEnsemble,
+  #     wave_ensemble_interval_s = Waves_Ensemble_Interval_s,
+  #     wave_averaging_interval_s = Waves_Averaging_Interval_s,
+  #     wave_pings_per_ensemble = Waves_PingsPerEnsemble
+  #   )
 }
